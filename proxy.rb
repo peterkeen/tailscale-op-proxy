ad5fc66bb3276d3373e22a483b8c08c363d08df9
Pete Keen <pete@petekeen.net>
2022-09-19 21:53:42 -0400

    hm

--- proxy.rb
+++ proxy.rb
@@ -39,6 +39,40 @@ class WhoisResponse < T::Struct
   const :UserProfile, UserProfile
 end
 
+class OPSection < T::Struct
+  const :id, String
+  const :label, T.nilable(String)
+end
+
+class OPField < T::Struct
+  const :id, String
+  const :type, String
+  const :purpose, String
+  const :label, String
+  const :value, String
+  const :section, T.nilable(OPSection)
+end
+
+class OPFile < T::Struct
+  const :id, String
+  const :name, String
+  const :size, Integer
+  const :content_path, String
+end
+
+class OPItem < T::Struct
+  const :id, String
+  const :title, String
+  const :tags, T::Array[String], default: []
+  const :vault, T::Hash[String, String]
+  const :category, String
+  const :sections, T::Array[OPSection], default: []
+  const :fields, T::Array[OPField], default: []
+  const :files, T::Array[OPFile], default: []
+  const :createdAt, DateTime
+  const :updatedAt, DateTime
+end
+
 class TailscaleOPProxy < Sinatra::Application
   def tailscale_whois(request)
     conn = Excon.new('unix:/local-tailscaled.sock', socket: '/var/run/tailscale/tailscaled.sock')
@@ -46,8 +80,23 @@ class TailscaleOPProxy < Sinatra::Application
     WhoisResponse.from_hash(JSON.parse(response.body))
   end
 
+  def all_secrets
+    conn = Excon.new('http://op-connect-api:8080', headers: {"Authorization" => "Bearer #{ENV['OP_CONNECT_API_TOKEN']}"})
+    response = conn.request(method: :get, path: "/v1/vaults/#{ENV['OP_CONNECT_VAULT_ID']}/items")
+
+    JSON.parse(response.body).map do |item|
+      item_resp = conn.request(method: :get, path: "/v1/vaults/#{ENV['OP_CONNECT_VAULT_ID']}/items/#{item['id']}")
+      OPItem.parse(item_resp.body)
+    end
+  end
+
+  def secrets_for_tags(tags)
+    tags = tags.dup.map { |t| t.gsub(/tag:/, '') }
+    all_secrets.select { |s| (s.tags & tags).length > 0 }
+  end
+
   get '/secrets' do
     whois = tailscale_whois(request)
-    whois.Node.Tags
+    secrets_for_tags(whois.Node.Tags)
   end
 end
